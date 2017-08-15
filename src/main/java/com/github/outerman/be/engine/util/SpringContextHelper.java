02c138389be95b0e0d1ed204674639a40223fa8c
gaoxue <gx@rrtimes.com>
2017-08-15 13:43:03 +0800

    格式化。

--- src/main/java/com/github/outerman/be/engine/util/SpringContextHelper.java
+++ src/main/java/com/github/outerman/be/engine/util/SpringContextHelper.java
@@ -15,10 +15,8 @@ public class SpringContextHelper implements ApplicationContextAware {
 
     private ApplicationContext context;
 
-
-    //提供一个接口，获取容器中的Bean实例，根据名称获取
-    public Object getBean(String beanName)
-    {
+    // 提供一个接口，获取容器中的Bean实例，根据名称获取
+    public Object getBean(String beanName) {
         return context.getBean(beanName);
     }
 
