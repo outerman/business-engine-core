880ef039ae58feedb495dc998d301a5a6f2fd108
gaox0326 <979307853@qq.com>
2018-03-20 10:19:28 +0800

    删除元数据相关 dto 和模板维护接口

--- src/main/java/com/github/outerman/be/engine/businessDoc/dataProvider/ITemplateProvider.java
+++ src/main/java/com/github/outerman/be/engine/businessDoc/dataProvider/ITemplateProvider.java
@@ -3,14 +3,10 @@ package com.github.outerman.be.engine.businessDoc.dataProvider;
 import java.util.List;
 import java.util.Map;
 
-import com.github.outerman.be.api.dto.BusinessAssetDto;
-import com.github.outerman.be.api.dto.BusinessAssetTypeDto;
 import com.github.outerman.be.api.vo.AcmSortReceiptDetail;
 import com.github.outerman.be.api.vo.DocAccountTemplateItem;
 import com.github.outerman.be.api.vo.FiAccount;
 import com.github.outerman.be.api.vo.PaymentTemplateItem;
-import com.github.outerman.be.api.vo.SetColumnsSpecialVo;
-import com.github.outerman.be.api.vo.SetColumnsTacticsDto;
 import com.github.outerman.be.api.vo.SetCurrency;
 
 /**
@@ -23,14 +19,6 @@ public interface ITemplateProvider {
     List<DocAccountTemplateItem> getBusinessTemplateByCode(Long orgId, String businessCode);
 
     List<PaymentTemplateItem> getPayTemplate(Long orgId, String businessCode);
-
-    Map<Long, List<SetColumnsTacticsDto>> getTacticsByCode(Long orgId, String businessCode);
-
-    Map<Long, List<SetColumnsSpecialVo>> getSpecialByCode(Long orgId, String businessCode);
-
-    List<BusinessAssetDto> getInventoryProperty(String businessCode);
-
-    List<BusinessAssetTypeDto> getAssetType(String businessCode);
 
     SetCurrency getBaseCurrency(Long orgId);
 
