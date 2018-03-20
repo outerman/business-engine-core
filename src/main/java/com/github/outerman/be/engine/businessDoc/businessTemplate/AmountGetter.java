cce42a6d33806e9e9d06b2c8576331d75d16d547
gaox0326 <979307853@qq.com>
2018-03-20 14:47:30 +0800

    删除不使用的代码

--- src/main/java/com/github/outerman/be/engine/businessDoc/businessTemplate/AmountGetter.java
+++ src/main/java/com/github/outerman/be/engine/businessDoc/businessTemplate/AmountGetter.java
@@ -1,9 +1,5 @@
 package com.github.outerman.be.engine.businessDoc.businessTemplate;
 
-import java.util.List;
-import java.util.Map;
-
-import com.github.outerman.be.engine.util.CommonUtil;
 import com.github.outerman.be.engine.util.JexlUtil;
 
 /**
@@ -13,55 +9,13 @@ import com.github.outerman.be.engine.util.JexlUtil;
  */
 public class AmountGetter {
 
-    private final static String MAOHAO = ":";
-    private final static String JINGHAO = "#";
-
-    private static String handleFundSource(String fundsource) {
-        if (!fundsource.contains(MAOHAO)) {
-            return fundsource;
-        }
-        String[] array1 = fundsource.split(MAOHAO);
-        if (array1.length != 3) {
-            return fundsource;
-        }
-        for (int index = 0, length = array1.length; index < length; index++) {
-            String str = array1[index];
-            if (!str.contains(JINGHAO)) {
-                continue;
-            }
-            String[] array2 = str.split(JINGHAO);
-            array1[index] = String.format("(%s ? %s : %s)", array2[0], array2[1], array2[2]);
-        }
-        return String.format("(%s ? %s : %s)", array1[0], array1[1], array1[2]);
-    }
-
-    public static boolean validateExpression(String fundsource) {
-        fundsource = handleFundSource(fundsource);
-        boolean result = JexlUtil.isExprValid(fundsource);
-        return result;
-    }
-
     public static <T> Double getAmount(T detail, String fundsource) {
-        fundsource = handleFundSource(fundsource);
         Object obj = JexlUtil.evaluate(fundsource, detail);
         if (obj == null) {
             return 0.0;
         }
         Double result = Double.parseDouble(obj.toString());
         return result;
-    }
-
-    public static List<String> getAmountFieldNameList(String fundsource) {
-        fundsource = handleFundSource(fundsource);
-        List<String> fieldNameList = JexlUtil.getIdentifierName(fundsource);
-        return fieldNameList;
-    }
-
-    public static String fundsource2Chinese(String fundsource) {
-        fundsource = handleFundSource(fundsource);
-        Map<String, String> map = CommonUtil.getFieldName2NameMap();
-        String parsedText = JexlUtil.getParsedText(fundsource, map);
-        return parsedText;
     }
 
 }
